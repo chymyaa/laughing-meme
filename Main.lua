6140a6746f4c4119d9d59bf5393802daba566656
chymyaa <angelaymy@gmail.com>
2026-03-10 20:44:17 +0800

    Add comments and additional loadstring calls

--- Main.lua
+++ Main.lua
@@ -1,1 +1,4 @@
+-- rivals
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4fd0a374461ccecc3532ce14ce12e58ac7386a841739aa10b0dfe7f2c3493a5e/download"))()
+-- flick
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/25ddb8a60bddd1986f4fa9e02e318eab33bc507751d43b6c7fdba1842a208521/download"))()
