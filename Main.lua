97f4dda5a64a89fd41789c9ac0137661cca3c3c6
chymyaa <angelaymy@gmail.com>
2026-03-14 14:11:53 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,13 @@
 -- better
 local GameList = {
     ["136801880565837"] = { 
-    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/25ddb8a60bddd1986f4fa9e02e318eab33bc507751d43b6c7fdba1842a208521/download"))()
-    }
+        url = "https://api.jnkie.com/api/v1/luascripts/public/25ddb8a60bddd1986f4fa9e02e318eab33bc507751d43b6c7fdba1842a208521/download"
+    },
     ["17625359962"] = { 
-    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4fd0a374461ccecc3532ce14ce12e58ac7386a841739aa10b0dfe7f2c3493a5e/download"))()
-        }
+        url = "https://api.jnkie.com/api/v1/luascripts/public/4fd0a374461ccecc3532ce14ce12e58ac7386a841739aa10b0dfe7f2c3493a5e/download"
+    }
+}
+
 local plr = game.Players.LocalPlayer
 local PlaceId = tostring(game.PlaceId)
 local entry = GameList[PlaceId]
@@ -14,6 +16,7 @@ if entry then
     if entry.keyless then
         loadstring(game:HttpGet(entry.url))()
     else
+        loadstring(game:HttpGet(entry.url))()
     end
 else
     plr:Kick("Game not supported")
