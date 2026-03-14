38a2e43c6e0af3a0e0f5469b6e442bfb335c1dc6
chymyaa <angelaymy@gmail.com>
2026-03-14 14:11:22 +0800

    Refactor GameList to load scripts dynamically

--- Main.lua
+++ Main.lua
@@ -1,17 +1,11 @@
 -- better
 local GameList = {
     ["136801880565837"] = { 
-        name = "Flick", 
-        url = "https://api.jnkie.com/api/v1/luascripts/public/25ddb8a60bddd1986f4fa9e02e318eab33bc507751d43b6c7fdba1842a208521/download",
-        keyless = false 
-    },
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/25ddb8a60bddd1986f4fa9e02e318eab33bc507751d43b6c7fdba1842a208521/download"))()
+    }
     ["17625359962"] = { 
-        name = "Rivals", 
-        url = "https://api.jnkie.com/api/v1/luascripts/public/4fd0a374461ccecc3532ce14ce12e58ac7386a841739aa10b0dfe7f2c3493a5e/download",
-        keyless = false 
-    },
-}
-
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4fd0a374461ccecc3532ce14ce12e58ac7386a841739aa10b0dfe7f2c3493a5e/download"))()
+        }
 local plr = game.Players.LocalPlayer
 local PlaceId = tostring(game.PlaceId)
 local entry = GameList[PlaceId]
